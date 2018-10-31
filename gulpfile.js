d3f76b52746dd94a99448c2492ca6fc9ccf4e636
Adeonir Kohl <adeonir@gmail.com>
2018-10-30 21:15:15 -0300

    Update npm packages and gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,90 +1,93 @@
-const gulp = require('gulp');
-const plumber = require('gulp-plumber');
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const cssnano = require('gulp-cssnano');
-const pug = require('gulp-pug');
-const babel = require('gulp-babel');
-const minify = require('gulp-terser');
-const imagemin = require('gulp-imagemin');
-const svgmin = require('gulp-svgmin');
-const browserSync = require('browser-sync');
-const del = require('del');
+const gulp = require('gulp')
+const changed = require('gulp-changed')
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+const cssnano = require('gulp-cssnano')
+const pug = require('gulp-pug')
+const babel = require('gulp-babel')
+const minify = require('gulp-terser')
+const imagemin = require('gulp-imagemin')
+const svgmin = require('gulp-svgmin')
+const browserSync = require('browser-sync').create()
+const del = require('del')
 
-gulp.task('clear', () => del('dist'));
+gulp.task('clear', () => del('dist'))
 
 gulp.task('styles', () =>
   gulp
-    .src('./src/styles/**/*')
-    .pipe(plumber())
+    .src('src/styles/**/*')
+    .pipe(change('dist/css'))
     .pipe(sass())
     .pipe(autoprefixer())
     .pipe(cssnano())
     .pipe(gulp.dest('dist/css'))
-    .pipe(browserSync.stream())
-);
+    .pipe(browserSync.stream()),
+)
 
 gulp.task('scripts', () =>
   gulp
-    .src('./src/scripts/**/*')
-    .pipe(plumber())
+    .src('src/scripts/**/*')
+    .pipe(change('dist/js'))
     .pipe(babel())
     .pipe(minify())
     .pipe(gulp.dest('dist/js'))
-    .pipe(browserSync.stream())
-);
+    .pipe(browserSync.stream()),
+)
 
 gulp.task('images', () =>
   gulp
-    .src('./src/images/**/*')
-    .pipe(plumber())
+    .src('src/images/**/*')
+    .pipe(change('dist/img'))
     .pipe(imagemin())
     .pipe(gulp.dest('dist/img'))
-    .pipe(browserSync.stream())
-);
+    .pipe(browserSync.stream()),
+)
 
 gulp.task('vectors', () =>
   gulp
-    .src('./src/vectors/**/*')
+    .src('src/vectors/**/*')
+    .pipe(change('dist/svg'))
     .pipe(svgmin())
-    .pipe(gulp.dest('./dist/svg/'))
-);
+    .pipe(gulp.dest('dist/svg/')),
+)
 
 gulp.task('pages', () =>
   gulp
-    .src('./src/pages/*.pug')
-    .pipe(plumber())
+    .src('src/pages/*.pug')
+    .pipe(change('dist'))
     .pipe(pug())
     .pipe(gulp.dest('dist'))
-    .pipe(browserSync.stream())
-);
+    .pipe(browserSync.stream()),
+)
 
 gulp.task('server', () => {
   browserSync.init({
-    server: 'dist',
-  });
+    server: {
+      baseDir: 'dist',
+    },
+  })
 
-  gulp.watch('./src/styles/**/*', gulp.series(['styles']));
-  gulp.watch('./src/scripts/**/*', gulp.series(['scripts']));
-  gulp.watch('./src/images/**/*', gulp.series(['images']));
-  gulp.watch('./src/vectors/**/*', gulp.series(['vectors']));
-  gulp.watch('./src/pages/**/*', gulp.series(['pages']));
-});
+  gulp.watch('src/styles/**/*', gulp.series(['styles']))
+  gulp.watch('src/scripts/**/*', gulp.series(['scripts']))
+  gulp.watch('src/images/**/*', gulp.series(['images']))
+  gulp.watch('src/vectors/**/*', gulp.series(['vectors']))
+  gulp.watch('src/pages/**/*', gulp.series(['pages']))
+})
 
 gulp.task(
   'dev',
   gulp.series(
     gulp.parallel('styles', 'scripts', 'images', 'vectors'),
     'pages',
-    'server'
-  )
-);
+    'server',
+  ),
+)
 
 gulp.task(
   'build',
   gulp.series(
     'clear',
     gulp.parallel('styles', 'scripts', 'images', 'vectors'),
-    'pages'
-  )
-);
+    'pages',
+  ),
+)
